235759aa366249dfba08303fea8df0cd32bfe625
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-10 16:08:09 +0100

    async redux set up

--- app/src/states/actionCreators.js
+++ app/src/states/actionCreators.js
@@ -1,4 +1,7 @@
+import axios from 'axios';
 import * as types from './actionTypes';
+
+const nasaApi = "https://api.nasa.gov/planetary/apod?api_key=YoeP5szYbSrnFTE4KUqS2aUbANxWq7flop3fbkBb";
 
 export function increment() {
     return {
@@ -17,3 +20,25 @@ export function reset() {
         type:types.RESET
     }
 }
+
+export function setNAPOD(data) {
+    console.log(data);
+    return {
+        type:types.NASAPOD,
+        payload: data
+    }
+}
+
+export const getNasaAPI = () => dispatch => {
+    axios.get(nasaApi)
+    .then(response => {
+        // debugger
+        const data = response.data;
+        console.log(response)
+        dispatch(setNAPOD(data));
+    })
+    .catch( error => {
+        // debugger
+    })
+}
+
